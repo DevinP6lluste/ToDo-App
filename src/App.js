396cd94a4dda4525672879edc3078e8c64585827
DevinP6lluste <devinpolluste@gmail.com>
2020-11-09 20:28:12 +0200

    Firebase deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
 import Todo from './Todo';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import db from './firebase';
+import firebase from 'firebase';
 
 function App() {
-  const [todos, setTodos] = useState(['Learn Investing', 'Travel a lot']);
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+
+  // when the app loads, we need to listen the database and fetch new todos as they get added/removed
+  useEffect(() => {
+    // fires when app.js loads
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => doc.data().todo))
+    })
+  }, []);
 
   const addTodo = (event) => {
     event.preventDefault(); // Stops the refresh
+
+db.collection('todos').add({
+  todo: input,
+  timestamp: firebase.firestore.FieldValue.serverTimestamp()
+})
+
     setTodos([...todos, input]);
     setInput(''); // Clear the input after submit
   }
